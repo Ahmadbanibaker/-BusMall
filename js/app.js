74cfbdfe3e64865092eb6d41e96ecc06567e7e4c
Ahmadbanibaker <ahmadmurad502@gmail.com>
2021-08-09 17:08:10 +0300

    fixed the img

--- js/app.js
+++ js/app.js
@@ -26,6 +26,9 @@ let all = [];
 let counter = 0;
 let numberOfRound = 25;
 let divE = document.getElementById('list');
+let imgname = [];
+let imgviwes = [];
+let imgclicks = [];
 
 const imageSection = document.getElementById('imageSection');
 let imgone = document.getElementById( 'imgone' );
@@ -58,9 +61,13 @@ let rightRandom ;
 let centerRandom ;
 
 function render() {
-  leftRandom = randomNumber( 0, imgArray.length - 1 );
-  rightRandom = randomNumber( 0, imgArray.length - 1 );
-  centerRandom = randomNumber(0, imgArray.length - 1);
+
+  do{
+    leftRandom = randomNumber(0, imgArray.length -1);
+    rightRandom = randomNumber(0, imgArray.length -1);
+    centerRandom = randomNumber(0, imgArray.length -1);
+  }
+  while (leftRandom === rightRandom || leftRandom === centerRandom || centerRandom === rightRandom);
 
   imgone.src = './img/' + Mall.all[leftRandom].image;
   imgtwo.src = './img/' + Mall.all[rightRandom].image;
@@ -70,7 +77,7 @@ function render() {
   Mall.all[rightRandom].shown++;
   Mall.all[centerRandom].shown++;
 
-  
+
 }
 
 render();
@@ -103,7 +110,7 @@ function whenclick(event){
 
 
 function clickbutton (){
-
+  drawchart();
   let ulE = document.createElement('ul');
   divE.appendChild(ulE);
 
@@ -118,3 +125,72 @@ function clickbutton (){
 
 
 
+let ctx = document.getElementById('myChart').getContext('2d');
+function drawchart(){
+  for(let i = 0; i < imgArray.length-1; i++){
+    imgname.push( Mall.all[i].name);
+    imgviwes.push(Mall.all[i].shown) ;
+    imgclicks.push(Mall.all[i].click) ;
+    console.log( Mall.all[i].name , Mall.all[i].shown , Mall.all[i].click );
+
+  }
+  let myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: imgname,
+      datasets: [{
+        label: '# of Viwes',
+        data: imgviwes,
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+      },
+      {
+        label: '# of clicks',
+        data: imgclicks,
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+      }
+      ]
+
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+}
+
+function 
